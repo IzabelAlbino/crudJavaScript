cb87215b55d61dd8d6bd5a0dfe5683e44b6a377a
Izabel Albino <izabelmoreira18@gmail.com>
2023-10-01 18:51:45 -0300

    Segundo commit - adicionada a função de excluir e editar cliente

--- script.js
+++ script.js
@@ -27,11 +27,19 @@ function displayClients() {
     const clientList = document.getElementById('clientList');
     clientList.innerHTML = '';
 
-    clients.forEach(client => {
+    clients.forEach((client, index) => {
         const listItem = document.createElement('li');
-        listItem.innerHTML = `<strong>ID:</strong> ${client.id}<br>
-                            <strong>Name:</strong> ${client.name}<br>
-                            <strong>Email:</strong> ${client.email}`;
+        listItem.innerHTML = 
+                            `<strong>ID:</strong> ${client.id}<br>
+
+                            <strong>Name:</strong>
+                             <input id="name" value="${client.name}"><br>
+
+                            <strong>Email:</strong> 
+                            <input id="email:" value="${client.email}"><br>
+
+                            <button onclick="deleteClient(${index})">🚮</button>
+                            <button onclick="updateClient(event, ${index})">📓</button>`;
         clientList.appendChild(listItem);
     });
 }
@@ -39,4 +47,28 @@ function displayClients() {
 function clearForm() {
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
+}
+
+function deleteClient(index) {
+    // Encontra o índice do cliente a ser excluído com base no ID
+    
+    clients.splice(index, 1);
+    displayClients();
+}
+
+
+function updateClient(event, index) {
+
+    const li = event.target.parentElement;
+    const name = li.querySelector('#name').value;
+    
+    const email = li.querySelector('#email').value;
+    const client = clients[index];
+
+    client.name = name;
+    client.email = email;
+
+    console.log('Valor atualizado:', client);
+
+
 }
